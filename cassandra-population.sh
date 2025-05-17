4c1b7727362b790171c11a111b0b4644e191f025
DavidIlnytskyi <ilnytskyy.d@gmail.com>
2025-05-17 18:43:01 +0300

    Add code to spark

--- cassandra-population.sh
+++ cassandra-population.sh
@@ -7,14 +7,67 @@ done
 echo "Cassandra is ready. Creating keyspace and table..."
 
 docker exec -i cassandra cqlsh -u cassandra -p uncommonpass <<EOF
-CREATE KEYSPACE IF NOT EXISTS wiki_data
-WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};
+CREATE KEYSPACE IF NOT EXISTS wiki_stream WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1};
 
-CREATE TABLE IF NOT EXISTS wiki_data.results (
-    user_id int,
-    domain text,
-    created_at timestamp,
-    page_title text,
-    PRIMARY KEY (user_id, created_at)
+CREATE TABLE IF NOT EXISTS wiki_stream.agg_one (
+    domain TEXT,
+    hour INT,
+    date date,
+    timestamp TIMESTAMP,
+    PRIMARY KEY ((domain, timestamp), date, hour)
+);
+
+CREATE TABLE IF NOT EXISTS wiki_stream.agg_two (
+    domain TEXT,
+    hour INT,
+    date DATE,
+    timestamp TIMESTAMP,
+    PRIMARY KEY ((domain, timestamp), date, hour)
+);
+
+CREATE TABLE IF NOT EXISTS wiki_stream.agg_three (
+    user_id BIGINT,
+    user_name TEXT,
+    timestamp TIMESTAMP,
+    date DATE,
+    hour INT,
+    page_title TEXT,
+    PRIMARY KEY ((user_id), timestamp)
+);
+
+CREATE TABLE IF NOT EXISTS wiki_stream.adhoc_one (
+    domain TEXT,
+    count COUNTER,
+    PRIMARY KEY (domain)
+);
+
+CREATE TABLE IF NOT EXISTS wiki_stream.adhoc_two (
+    user_id BIGINT,
+    timestamp TIMESTAMP,
+    url TEXT,
+    PRIMARY KEY(user_id, timestamp)
+);
+
+
+CREATE TABLE IF NOT EXISTS wiki_stream.adhoc_three (
+    domain TEXT,
+    timestamp TIMESTAMP,
+    url TEXT,
+    PRIMARY KEY((domain, url), timestamp)
+);
+
+CREATE TABLE IF NOT EXISTS wiki_stream.adhoc_four (
+    page_id BIGINT,
+    timestamp timestamp,
+    url TEXT,
+    PRIMARY KEY((page_id, url), timestamp)
+);
+
+
+CREATE TABLE IF NOT EXISTS wiki_stream.adhoc_five (
+    user_id BIGINT,
+    user_name TEXT,
+    timestamp TIMESTAMP,
+    PRIMARY KEY ((user_id, timestamp), hour)
 );
 EOF
--- spark/submain.py
+++ spark/submain.py
@@ -1,17 +1,6 @@
+from pyspark.sql.functions import col, from_json, to_timestamp, hour, expr, lit, decode
+from pyspark.sql.types import StructType, StructField, StringType, LongType, IntegerType, BooleanType, TimestampType, MapType, StructType
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, from_json, decode, to_timestamp
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType, BooleanType
-
-full_schema = StructType([
-    StructField("meta", StructType([
-        StructField("domain", StringType()),
-        StructField("dt", StringType())
-    ])),
-    StructField("performer", StructType([
-        StructField("user_id", IntegerType())
-    ])),
-    StructField("page_title", StringType())
-])
 
 spark = SparkSession.builder \
     .appName("KafkaToCassandra") \
@@ -32,21 +21,107 @@ kafka_df = spark.readStream \
 
 json_df = kafka_df.select(decode(col("value"), "utf-8").alias("json_string"))
 
-parsed_df = json_df.select(from_json(col("json_string"), full_schema).alias("data"))
+performer_schema = StructType([
+    StructField("user_text", StringType()),
+    StructField("user_groups",
+                StringType(), True),
+    StructField("user_is_bot", BooleanType()),
+    StructField("user_id", LongType()),
+    StructField("user_registration_dt", StringType()),
+    StructField("user_edit_count", LongType())
+])
 
-result_df = parsed_df.select(
-    col("data.performer.user_id").alias("user_id"),
+rev_slots_schema = StructType([
+    StructField("main", StructType([
+        StructField("rev_slot_content_model", StringType()),
+        StructField("rev_slot_sha1", StringType()),
+        StructField("rev_slot_size", LongType()),
+        StructField("rev_slot_origin_rev_id", LongType())
+    ]))
+])
+
+meta_schema = StructType([
+    StructField("uri", StringType()),
+    StructField("request_id", StringType()),
+    StructField("id", StringType()),
+    StructField("dt", StringType()),
+    StructField("domain", StringType()),
+    StructField("stream", StringType()),
+    StructField("topic", StringType()),
+    StructField("partition", IntegerType()),
+    StructField("offset", LongType())
+])
+
+schema = StructType([
+    StructField("$schema", StringType()),
+    StructField("meta", meta_schema),
+    StructField("database", StringType()),
+    StructField("page_id", LongType()),
+    StructField("page_title", StringType()),
+    StructField("page_namespace", IntegerType()),
+    StructField("rev_id", LongType()),
+    StructField("rev_timestamp", StringType()),
+    StructField("rev_sha1", StringType()),
+    StructField("rev_minor_edit", BooleanType()),
+    StructField("rev_len", LongType()),
+    StructField("rev_content_model", StringType()),
+    StructField("rev_content_format", StringType()),
+    StructField("performer", performer_schema),
+    StructField("page_is_redirect", BooleanType()),
+    StructField("comment", StringType()),
+    StructField("parsedcomment", StringType()),
+    StructField("dt", StringType()),
+    StructField("rev_slots", rev_slots_schema)
+])
+
+parsed_df = json_df.select(from_json(col("json_string"), schema).alias("data"))
+
+flat_df = parsed_df.select(
     col("data.meta.domain").alias("domain"),
-    to_timestamp(col("data.meta.dt")).alias("created_at"),
-    col("data.page_title").alias("page_title")
-)
+    to_timestamp("data.dt").alias("timestamp"),
+    col("data.performer.user_id").alias("user_id"),
+    col("data.performer.user_text").alias("user_name"),
+    col("data.meta.uri").alias("url"),
+    col("data.page_id").alias("page_id"),
+    col("data.page_title").alias("page_title"),
+    col("data.performer.user_is_bot").alias("user_is_bot")
+).withColumn("date", col("timestamp").cast("date")) \
+ .withColumn("hour", hour("timestamp")) \
+ .withColumn("count", lit(1))
 
-query = result_df.writeStream \
-    .format("org.apache.spark.sql.cassandra") \
-    .option("keyspace", "wiki_data") \
-    .option("table", "results") \
-    .option("checkpointLocation", "/tmp/spark_checkpoint/kafka_to_cassandra") \
-    .outputMode("append") \
-    .start()
 
-query.awaitTermination()
+def write_to_cassandra(df, table):
+    df.writeStream \
+        .format("org.apache.spark.sql.cassandra") \
+        .option("checkpointLocation", f"/tmp/checkpoints/{table}") \
+        .option("keyspace", "wiki_stream") \
+        .option("table", table) \
+        .outputMode("append") \
+        .start()
+
+agg_df = flat_df.select("domain", "hour", "date", "timestamp")
+write_to_cassandra(agg_df, "agg_one")
+
+agg_two_df = flat_df.filter(col("user_is_bot") == True) \
+    .select("domain", "hour", "date", "timestamp")
+write_to_cassandra(agg_two_df, "agg_two")
+
+agg_three_df = flat_df.select("user_id", "user_name", "timestamp", "date", "hour", "page_title")
+write_to_cassandra(agg_three_df, "agg_three")
+
+adhoc_one_df = flat_df.select("domain", "count")
+write_to_cassandra(adhoc_one_df, "adhoc_one")
+
+adhoc_two_df = flat_df.select("user_id", "timestamp", "url")
+write_to_cassandra(adhoc_two_df, "adhoc_two")
+
+adhoc_three_df = flat_df.select("domain", "timestamp", "url")
+write_to_cassandra(adhoc_three_df, "adhoc_three")
+
+adhoc_four_df = flat_df.select("page_id", "timestamp", "url")
+write_to_cassandra(adhoc_four_df, "adhoc_four")
+
+adhoc_five_df = flat_df.select("user_id", "user_name", "timestamp", "hour")
+write_to_cassandra(adhoc_five_df, "adhoc_five")
+
+spark.streams.awaitAnyTermination()
