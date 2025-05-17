f2ab72fd89a9b35578d762faa5d5ed4782490a5d
DavidIlnytskyi <ilnytskyy.d@gmail.com>
2025-05-17 23:14:54 +0300

    Update run-cluster script

--- run-cluster.sh
+++ run-cluster.sh
@@ -4,3 +4,9 @@ docker compose up -d --build
 
 bash kafka-population.sh
 bash cassandra-population.sh
+
+bash docker exec -i spark spark-submit --master local[1] --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0 /opt/bitnami/spark/data/main.py
+
+bash cassandra-population.sh
+
+bash docker exec -i spark spark-submit --master local[1] --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0,com.datastax.spark:spark-cassandra-connector_2.12:3.5.0 --conf spark.cassandra.connection.host=cassandra --conf spark.cassandra.auth.username=cassandra --conf spark.cassandra.auth.password=uncommonpass /opt/bitnami/spark/data/submain.py
